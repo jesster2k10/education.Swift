4c33182092bad0b98d0b628251e1d89f198f0b27
zlobo <zlobo@MacBook-Air-FHouseMac.local>
2015-11-06 19:43:55 +0300

    calculator 06.11.15

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-var tempValue : Int = 0
+var tempOneValue : Int = 0
+var tempTwoValue : Int = 0
 var tempSign : String = ""
+var equalCheck : Bool = false
+
+
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,27 +27,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var titleButtons: UIButton!
+    @IBOutlet weak var displayLabel: UITextField!
+    
+    
     
     @IBAction func allButton(sender : UIButton) {
+        let display : Int? = Int(displayLabel.text!)
+        
         if let text = sender.titleLabel?.text {
             switch(text) {
             case "1","2","3","4","5","6","7","8","9","0": displayLabel.text?.appendContentsOf(text)
-            case "+", "-": tempValue = Int(displayLabel.text!)!
+            case "+", "-": tempOneValue = display!
                            tempSign = sender.titleLabel!.text!
-                           displayLabel.text = ""
+            
+            //TODO
+            displayLabel.
+                           equalCheck = false
             case "C": displayLabel.text = ""
-            default: displayLabel.text = calculate(tempValue, Int(displayLabel.text!)! ,tempSign)
+            default: if (!equalCheck) {
+                        tempTwoValue = display!
+                        displayLabel.text = calculate(tempOneValue, tempTwoValue ,tempSign)
+                        equalCheck = true }
+                     else {
+                        displayLabel.text = calculate(display!, tempTwoValue ,tempSign)
+                     }
             }
         }
     }
 }
 
 func calculate(oldValue : Int, _ newValue : Int, _ sign : String) -> String {
-    tempValue = oldValue
     switch(sign) {
         case "+": return String(newValue + oldValue)
+        case "-": return String(oldValue - newValue)
         default: return "0"
     }
 }
